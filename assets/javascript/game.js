d9922ded6ef3c704aa4bc36e740cfe98555d2285
mgulham <mgulham@yahoo.com>
2019-07-28 14:37:33 -0700

    After timer ends, scores are pushed to final screen. User given option to restart game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,21 +10,25 @@ function pushQuestions(){
     $("#questions").html("Hello")
 }
 function startGame(){
+
 setTimeout(pushQuestions, 1000);
+
 $(".mainHeaderHello").empty();
+
 var timeleft = 3;
+
 var downloadTimer = setInterval(function(){
   document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
   timeleft--;
   if(timeleft < 0){
     clearInterval(downloadTimer);
     document.getElementById("countdown").innerHTML = "Finished"
-    $(".body").append('<div class= "gameOver">YOUR TIME IS UP! Refresh to try again.</div>');
+    $(".body").append('<div class="gameOver"><a href= "https://mgulham.github.io/trivia-game/" style="color: red">YOUR TIME IS UP! Click here to try again.</a></div>');
     $("#questions").empty();
-    $("#questions").append(`Correct:${correct},
-    Incorrect:${incorrect},
-     Unanswered:${unanswered},
-     Score:${score}`)
+    
+    $("#questions").append(`<p class= "endScore">Correct:${correct}</p>
+      <p class= "endScore">Incorrect:${incorrect}</p> 
+      <p class= "endScore">Unanswered:${unanswered}</p>`)
     
   }
 }, 1000);
